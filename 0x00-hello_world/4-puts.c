96eb96077274ac3f28a11ae7e6ba5e896ba68ce6
KehindeALX <kehindeadesunloye2@gmail.com>
2023-06-19 23:27:04 +0100

    0-positive_or_negative.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-/**
-* main - Entry
-* Return: Always 0
-*/
-int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-}	
+my file
--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,31 @@
 #include <time.h>
 #include <stdlib.h>
 
+
 /**
   * main - Determines if a number is positive, negative or zero
   * description: get a random number and print number
   * Return: Always 0 (success)
   */
 int main(void)
-{
+
+ {
         int n;
         srand(time(0));
         n = rand() - RAND_MAX / 2;
+
         if n(n >0)
+	{
                 printf("%i is positive\n", n);
+	}	
         else if (n < 0)
+	{	
                 printf("%i is negative\n", n);
+	}	
         else
+	{	
                 printf("%i is zero\n", n);
+	}	
         return (0);
-}
+
+ }
