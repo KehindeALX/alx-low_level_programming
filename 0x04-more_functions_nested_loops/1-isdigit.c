6d9c8375915541bb2ac9e3cde239f282ce3d7a4d
KehindeALX <kehindeadesunloye2@gmail.com>
2023-06-23 02:00:14 +0100

    update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+
+/**
+ * main - write to check for a digit
+ *
+ * Return: Always 0.
+
+int _isdigit(int c)
+{
+   char c  = '6';
+   if (isdigit (c))
+   {
+   printf("%c is a digit\n", c);
+   }
+   else
+   {
+   printf("%c is not a digit\n", c);
+   }
+   return 0; 
+}
